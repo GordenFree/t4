a8c6d41f489fc640949d02b079c48a69e7f1b4a9
GordenFree <90825860+GordenFree@users.noreply.github.com>
2021-12-24 15:34:08 +0700

    24.12.2021

--- t4/main/models.py
+++ t4/main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Bboard(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Модель автомобиля')
+    title = models.CharField(max_length=50, verbose_name='Модель автомобиля') 
     content = models.TextField(null=True, blank=True, verbose_name='Описание авто', help_text='Напишите об автомобиле')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена автомобиля')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    
     class Meta:
         verbose_name='Обьявление'
         verbose_name_plural='Обьявления'
@@ -19,4 +20,4 @@ class Rubric(models.Model):
     class Meta:
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
-        ordering = ['name']        
+        ordering = ['name']
--- t4/main/tests.py
+++ t4/main/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+def apply_discount(product, discount):
+    price = int(product['цена'] * (1.0 - discount))
+    assert 0 <= price <= product['цена']
+    return price
 
-# Create your tests here.
+shoes = {'имя': 'модные туфли', 'цена': 15690}
+
+print (apply_discount (shoes, 1))
+
+with open('hello.txt', 'w') as f:
+    f.write('привет мир')
