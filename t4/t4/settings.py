0117b221ebe6cbe0dc053434ea6896e5bbb16bf3
GordenFree <90825860+GordenFree@users.noreply.github.com>
2021-12-29 15:36:40 +0700

    Create settings.py

--- t4/t4/settings.py
+++ t4/t4/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xj-dq6*2^veop)7(biu)le%a(9#tm@v)!pkhfppw$tn809n3kr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
